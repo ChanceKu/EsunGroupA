e102954287b33cc13fde1816790bca4453fb4c12
ChanceKu <kuhonman609@gmail.com>
2023-04-09 07:02:30 +0800

    finished plot.py version 1
    combined2.ipynb will creates .csv in a folder and plot.py will load all the .csv in the folder and plot it and save it as .png
    
    Unfinished:
    make it importable for combine2.ipynb
    documentation to be made

---
+++ test.py
@@ -0,0 +1,9 @@
+import matplotlib
+print(matplotlib.__file__)
+
+import matplotlib.font_manager
+ 
+a = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
+ 
+for i in a:
+    print(i)
